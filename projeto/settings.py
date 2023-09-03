eec14d533053fb8e009aa2815a3b7b308bf6e457
pedrolisboaa <phlisboa2000@gmail.com>
2023-09-03 19:57:17 -0300

    atulizando de novo

--- projeto/settings.py
+++ projeto/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 #SECRET_KEY = '123'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
