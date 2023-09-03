67df2978d021ef61e02eb15a460743a12a598f57
pedrolisboaa <phlisboa2000@gmail.com>
2023-09-03 20:12:37 -0300

    testando novamente

--- staticfiles/js/script.js
+++ staticfiles/js/script.js
@@ -1,7 +1,10 @@
 document.querySelector('form').addEventListener('submit', function(evento) {
     let cpfInput = document.getElementById('input-cpf');
+    console.log(cpfInput)
     let cpfValue = cpfInput.value.replace(/[.-]/g, '');
+    console.log(cpfValue)
     cpfInput.value = cpfValue;
 });
 
-console.log('oi!')
+
+
