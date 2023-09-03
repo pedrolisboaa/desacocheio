8c5bb2183b9da5acc93109d20513684931dc9fbc
pedrolisboaa <phlisboa2000@gmail.com>
2023-09-03 20:07:02 -0300

    arrumando o JS do cpf

--- staticfiles/js/script.js
+++ staticfiles/js/script.js
@@ -1,7 +1,7 @@
-document.getElementById('btn-cpf').addEventListener('submit', function(evento){
-    let cpfInput = document.getElementById('btn-cpf')
-    let limpandoCpf = cpfInput.value.replace(/[.-]/g, '')
-    cpfInput.value = limpandoCpf
-})
+document.querySelector('form').addEventListener('submit', function(evento) {
+    let cpfInput = document.getElementById('input-cpf');
+    let cpfValue = cpfInput.value.replace(/[.-]/g, '');
+    cpfInput.value = cpfValue;
+});
 
 console.log('oi!')
