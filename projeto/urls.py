e48978bf3d3508ae94576210b2a0a2e75a21a60b
pedrolisboaa <phlisboa2000@gmail.com>
2023-09-03 19:51:15 -0300

    atualizando site

--- projeto/urls.py
+++ projeto/urls.py
@@ -25,6 +25,6 @@ urlpatterns = [
     path('', include('cupom.urls')),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+#urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += staticfiles_urlpatterns()
